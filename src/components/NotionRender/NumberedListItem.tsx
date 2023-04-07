d7cb624ef234fba73555456df348bdbc9a1c1042
gooney <gooney.me@outlook.com>
2023-04-07 23:05:52 +0800

    style: adjust styles

--- src/components/NotionRender/NumberedListItem.tsx
+++ src/components/NotionRender/NumberedListItem.tsx
@@ -9,7 +9,7 @@ const NumberedListItem: React.FC<NumberedListItemProps> = ({ block }: NumberedLi
   numberedListItem += 1
   return (
     <div className="flex items-center ">
-      <div className="flex py-1 items-center justify-center text-gray-700  dark:text-gray-400 mr-2">{numberedListItem}.</div>
+      <div className="flex py-1 items-center justify-center text-gray-700  dark:text-gray-400 mx-2">{numberedListItem}.</div>
       <div className="flex-1">
         <RichText rich_text={block.numbered_list_item.rich_text}></RichText>
       </div>
