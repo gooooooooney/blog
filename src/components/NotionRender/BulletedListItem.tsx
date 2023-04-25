a8324490ace2628c92499cc77bbbaecaf175ecdb
张文光 <wenguangzhang@caixin.com>
2023-04-25 12:01:50 +0800

    style(custom): code component width 100%

--- src/components/NotionRender/BulletedListItem.tsx
+++ src/components/NotionRender/BulletedListItem.tsx
@@ -13,7 +13,7 @@ const BulletedListItem = async ({block} : BulletedListItemProps ) => {
     const children = await getBlocks(block.id)
     return (
       <div className="flex items-center pl-4">
-        <div className="flex-1 relative before:content-['-'] before:text-[#999] before:absolute before:-ml-4">
+        <div className="flex-1 relative before:content-['-'] w-full before:text-[#999] before:absolute before:-ml-4">
           <RichText  rich_text={block.bulleted_list_item.rich_text}></RichText>
           <div className="ml-[1em]">
             {
